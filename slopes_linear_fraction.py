1fb7d272f2d64548d776ce7a6084fa3f20004c6b
sashital <48926692+sashital@users.noreply.github.com>
2024-01-17 10:11:45 -0600

    Update slopes_linear_fraction.py

--- slopes_linear_fraction.py
+++ slopes_linear_fraction.py
@@ -1,15 +1,17 @@
 # This script takes the mean value abundance scores for nicked and SC fractions and determines the fraction of DNA that was not nicked or supercoiled (i.e. linearized)
 # It then determines a slope based on the fraction linear at the four Mg2+ concentrations
 # It outputs both the fraction linear values at each Mg2+ concentration and the slopes. The latter can be used to generate the slope heatmaps
-# Be sure to entier the directory and filenames for the two means files.
+# It can also be used to calculate first strand cleavage slopes, by uncommenting line 52
+# Be sure to enter the directory and filenames for the two means files.
 
 import os
 from scipy.stats import linregress
+import matplotlib.pyplot as plt
 
 # Set directory and filenames
-directory = '' # enter the name of the input/output directory
-file1_name = '' # enter the name for the mean abundance file for the nicked fraction
-file2_name = '' # enter the name for the mean abundance file for the SC faction
+directory = "C:/Users/sashital/Box/Manuscripts/Cas12a_metal-dependent_specificity/figure_workshop/231220_libraries/plasmid_library/linear_input"
+file1_name = "Fn_L_30_nicked_means.txt"
+file2_name = "Fn_L_30_SC_means.txt"
 
 # Extract prefix from input filenames
 prefix = '_'.join(file1_name.split('_')[:3])
@@ -45,7 +47,9 @@ combined_values = []
 for seq in sequences:
     value1 = file1_dict.get(seq, [0]*4)  # Get values for sequence from file 1
     value2 = file2_dict.get(seq, [0]*4)  # Get values for sequence from file 2
-    combined_value = [max(0, 1 - (value1[i] + value2[i])) for i in range(4)] # Calculate combined value for each column
+    #combined_value = [max(0, 1 - (value1[i] + value2[i])) for i in range(4)] # Calculate second strand cleavage, set negative values to 0
+    combined_value = [1 - (value1[i] + value2[i]) for i in range(4)] # Calculate second strand cleavage, keep negative values
+    #combined_value = [1 - (value2[i]) for i in range(4)] # Calculate first strand cleavage 
     combined_values.append([seq] + combined_value)
 
 
@@ -55,17 +59,27 @@ x = [1, 2, 5, 10]
 
 # Calculate slopes and write to output file 1
 slopes = []
+r_values = []
 for row in combined_values:
     y = [row[col] for col in range(1, 5)]
     slope, _, r_value, _, _ = linregress(x, y)
     slopes.append(slope)
+    r_values.append(r_value ** 2)
     
-with open(os.path.join(directory, f"{prefix}_lin_slopes.txt"), 'w') as outfile:
+with open(os.path.join(directory, f"{prefix}_lin_slopes_first.txt"), 'w') as outfile:
     for sequence, slope in zip(sequences, slopes):
         outfile.write(f"{sequence}\t{slope}\n")
 
-# Write the combined data to a new file
-with open(os.path.join(directory, f"{prefix}_lin_means.txt"), 'w') as outfile:
-    # Write data
-    for row in combined_values:
-        outfile.write('\t'.join([str(x) for x in row]) + '\n')
+# Write the combined data along with slopes and R² values to the existing file
+with open(os.path.join(directory, f"{prefix}_lin_means_first.txt"), 'w') as outfile:
+    for row, slope, r_value in zip(combined_values, slopes, r_values):
+        outfile.write('\t'.join([str(x) for x in row]) + f'\t{slope}\t{r_value}\n')
+        
+# # Scatter plot of slope versus R²
+# plt.scatter(slopes, r_values, s = 10)
+# plt.title("Slope versus R²")
+# plt.ylabel("R² Values")
+# plt.xlabel("Slopes")
+# plt.xlim(-0.1,0.1)
+# plt.show()
+
